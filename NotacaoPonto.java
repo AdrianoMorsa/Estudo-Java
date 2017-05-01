c9bd228a698c1d405af77f291af8a121fc3e0b4b
AdrianoMorsa <adrianomoreira84@hotmail.com>
2017-05-01 00:49:51 -0300

    NotacaoPonto -2

--- NotacaoPonto.java
+++ NotacaoPonto.java
@@ -2,7 +2,9 @@ package Fundamentos;
 
 public class NotacaoPonto {
 	public static void main(String[] args) {
-		String s = "Bom dia ?".toUpperCase().replace("?", "Bia").concat("!");
+		String s = "Bom dia ?".toUpperCase();
+		s = s. replace("?", "Bia");
+	    s = s. concat("!");
 		System.out.println(s);
 		
 	}
