b0023f58ab4f3625d81131146c9e01e45ef14edc
AdrianoMorsa <adrianomoreira84@hotmail.com>
2017-05-01 00:45:28 -0300

    ToUppercase and Reclace

---
+++ NotacaoPonto.java
@@ -0,0 +1,10 @@
+package Fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		String s = "Bom dia ?".toUpperCase().replace("?", "Bia").concat("!");
+		System.out.println(s);
+		
+	}
+
+}
