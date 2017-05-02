63e405fbcdeb285c08846c14d514b96bdce002b5
AdrianoMorsa <adrianomoreira84@hotmail.com>
2017-05-02 11:51:50 -0300

    Add files via upload

---
+++ ObjetosPrimitivos.java
@@ -0,0 +1,20 @@
+package Fundamentos;
+
+public class ObjetosPrimitivos {
+public static void main(String[] args) {
+	
+	Character c = 'a';
+	Boolean bo = true;
+	
+	Byte b = 1;
+	Short s = 2;
+	Integer i = 3;
+	Long l = 4L;
+	
+	Float f = 5.1f;
+	Double d = 6.1;
+	
+	System.out.println(bo.toString().length());
+	System.out.println(bo.toString().length());
+}
+}
