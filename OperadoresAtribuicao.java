a6c7e3ae40d19d7658c0680f9648743e6c59b53e
AdrianoMorsa <adrianomoreira84@hotmail.com>
2017-05-02 17:51:01 -0300

    Operadores Atribuição

---
+++ OperadoresAtribuicao.java
@@ -0,0 +1,17 @@
+package Fundamentos;
+
+public class OperadoresAtribuicao {
+
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 3;
+		
+		b += a; // b = b + a
+		b *= 4; // b = b * a
+		b /= 2; // b = b / 2
+		b -= 2; // b = b - 2
+		// b %= 2;
+		
+		System.out.println(b);
+	}
+}
