219c9f037038fe3edfe244677bc62ef492883cdd
AdrianoMorsa <adrianomoreira84@hotmail.com>
2017-05-02 17:23:04 -0300

    Operadores Aritiméticos

---
+++ OperadoresAritimeticos.java
@@ -0,0 +1,23 @@
+package Fundamentos;
+
+public class OperadoresAritimeticos {
+	
+public static void main(String[] args) {
+	
+	int num1 = 3;
+	int num2 = 4;
+	int num3 = 5;
+	
+	//Operadores: + _ / %
+	int soma = num1 + num2 + num3 + 6;
+	System.out.println(soma);
+	System.out.println(num1 - num2 - num3);
+	
+	//Cuidado com a precedência
+	System.out.println(num1 + num3 * num2/ 2);
+	System.out.println((num1 + num3) * num2 / 2);
+	
+	//Resto da divisão
+	System.out.println(10 % 3);
+}
+}
